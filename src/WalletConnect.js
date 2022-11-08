c7964f23b9365ced19e26ca63105245e2f12d453
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2022-11-08 12:32:15 +0530

    initial commit

---
+++ src/WalletConnect.js
@@ -0,0 +1,8 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+
+function WalletConnect() {
+  return <ConnectButton label="Connect Your Wallet" />;
+}
+
+export default WalletConnect;
